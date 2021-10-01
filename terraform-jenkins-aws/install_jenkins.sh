6e7e16eb87c810911a96b3e59d96f577421b9781
vikas99341 <48900898+vikas99341@users.noreply.github.com>
2021-10-01 08:24:26 +0530

    updated the script

--- terraform-jenkins-aws/install_jenkins.sh
+++ terraform-jenkins-aws/install_jenkins.sh
@@ -24,9 +24,8 @@ echo "Install Jenkins"
 wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo --no-check-certificate
 rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
 amazon-linux-extras install epel -y 
-yum install -y jenkins
+yum install jenkins -y
 sudo usermod -a -G docker jenkins
 sudo chkconfig jenkins on
 sudo service docker start
 sudo service jenkins start
-
